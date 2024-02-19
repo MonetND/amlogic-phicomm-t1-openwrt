f7329324da10cc47a76564e12d762831bdd8921f
MonetND <45937083+MonetND@users.noreply.github.com>
2024-02-19 18:25:33 +0800

    Update diy-part2.sh

--- config/openwrt-main/diy-part2.sh
+++ config/openwrt-main/diy-part2.sh
@@ -8,12 +8,49 @@
 
 # ------------------------------- Main source started -------------------------------
 #
+
+echo "开始 DIY2 配置……"
+echo "========================="
+
+function merge_package(){
+    repo=`echo $1 | rev | cut -d'/' -f 1 | rev`
+    pkg=`echo $2 | rev | cut -d'/' -f 1 | rev`
+    # find package/ -follow -name $pkg -not -path "package/custom/*" | xargs -rt rm -rf
+    git clone --depth=1 --single-branch $1
+    mv $2 package/custom/
+    rm -rf $repo
+}
+function drop_package(){
+    find package/ -follow -name $1 -not -path "package/custom/*" | xargs -rt rm -rf
+}
+function merge_feed(){
+    if [ ! -d "feed/$1" ]; then
+        echo >> feeds.conf.default
+        echo "src-git $1 $2" >> feeds.conf.default
+    fi
+    ./scripts/feeds update $1
+    ./scripts/feeds install -a -p $1
+}
+rm -rf package/custom; mkdir package/custom
+
 # Add the default password for the 'root' user（Change the empty password to 'password'）
 sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7:::/g' package/base-files/files/etc/shadow
 
 # Set etc/openwrt_release
 sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package/base-files/files/etc/openwrt_release
 echo "DISTRIB_SOURCECODE='official'" >>package/base-files/files/etc/openwrt_release
+
+merge_package https://github.com/vernesong/OpenClash OpenClash/luci-app-openclash
+pushd package/custom/luci-app-openclash/tools/po2lmo
+make && sudo make install
+popd
+
+git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
+merge_package https://github.com/thinktip/luci-theme-neobird luci-theme-neobird
+merge_package https://github.com/Leo-Jo-My/luci-theme-opentomcat luci-theme-opentomcat
+merge_package https://github.com/Leo-Jo-My/luci-theme-opentomato luci-theme-opentomato
+merge_package https://github.com/rosywrt/luci-theme-rosy luci-theme-rosy
+
 
 # Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.31.4）
 # sed -i 's/192.168.1.1/192.168.31.4/g' package/base-files/files/bin/config_generate
@@ -25,13 +62,14 @@ echo "DISTRIB_SOURCECODE='official'" >>package/base-files/files/etc/openwrt_rele
 # Add luci-app-amlogic
 rm -rf package/luci-app-amlogic
 git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
-merge_package https://github.com/vernesong/OpenClash OpenClash/luci-app-openclash
-
-pushd package/luci-app-openclash/tools/po2lmo
-make && sudo make install
-popd
 #
 # Apply patch
 # git apply ../config/patches/{0001*,0002*}.patch --directory=feeds/luci
 #
 # ------------------------------- Other ends -------------------------------
+
+./scripts/feeds update -a
+./scripts/feeds install -a
+
+echo "========================="
+echo " DIY2 配置完成……"
