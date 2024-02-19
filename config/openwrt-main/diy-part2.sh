9279447c98b113a8d173895fe58e8c47fbdce183
MonetND <45937083+MonetND@users.noreply.github.com>
2024-02-19 16:31:30 +0800

    Update diy-part2.sh

--- config/openwrt-main/diy-part2.sh
+++ config/openwrt-main/diy-part2.sh
@@ -25,6 +25,11 @@ echo "DISTRIB_SOURCECODE='official'" >>package/base-files/files/etc/openwrt_rele
 # Add luci-app-amlogic
 rm -rf package/luci-app-amlogic
 git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
+merge_package https://github.com/vernesong/OpenClash OpenClash/luci-app-openclash
+
+pushd package/luci-app-openclash/tools/po2lmo
+make && sudo make install
+popd
 #
 # Apply patch
 # git apply ../config/patches/{0001*,0002*}.patch --directory=feeds/luci
